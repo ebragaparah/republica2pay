ed32aa4a848b1ec7d9512036d613f3324b2fcc7c
henrique2010 <henrique.passalini@gmail.com>
2012-06-25 00:23:39 -0300

    verificação de confirmação de cadastro

--- spec/acceptance/cadastro_spec.rb
+++ spec/acceptance/cadastro_spec.rb
@@ -8,7 +8,8 @@ feature "cadastro" do
 
     logar(user)
     page.should have_content 'Você deve confirmar a sua conta antes de continuar.'
-
     
+    ActionMailer::Base.deliveries.last.should_not be_nil
+    ActionMailer::Base.deliveries.last.body.should have_link('Confirm')
   end
 end
