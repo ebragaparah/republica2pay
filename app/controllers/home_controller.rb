5f908df30e1a868f89dfe6818d3441da79e30370
henrique2010 <henrique.passalini@gmail.com>
2012-07-10 12:57:40 -0300

    adicionado redirect para a republica caso user logado e tenha republica

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to republica_path(current_user.republica) if current_user.republica
+    end
   end
 end
