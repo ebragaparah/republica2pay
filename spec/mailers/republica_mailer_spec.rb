a8911a941c26f64614e897272a9a39046e0087f5
henrique2010 <henrique.passalini@gmail.com>
2012-07-16 12:20:08 -0300

    deve ser o mesmo email em cada teste!

--- spec/mailers/republica_mailer_spec.rb
+++ spec/mailers/republica_mailer_spec.rb
@@ -3,23 +3,21 @@ require 'spec_helper'
 
 describe RepublicaMailer do
   describe "#enviar_convite" do
-    before(:each) do
-      @convidado = create :convidado
-      @convite = create :convite, convidados: [@convidado]
-      @user = create :user_com_republica
-      @mail = RepublicaMailer.enviar_convite(@convite, @convidado, @user)
-    end
+    let(:convidado) { create :convidado }
+    let(:convite) { create :convite, convidados: [convidado] }
+    let(:user) { create :user_com_republica }
+    let(:mail) { RepublicaMailer.enviar_convite(convite, convidado, user) }
 
     it "renderizar o cabeçalho" do
-      @mail.subject.should eq("#{@user.email} te convidou para republica #{@user.republica.nome}")
-      @mail.to.should eq([@convidado.email])
-      @mail.from.should eq([@user.email])
+      mail.subject.should eq("#{user.email} te convidou para republica #{user.republica.nome}")
+      mail.to.should eq([convidado.email])
+      mail.from.should eq([user.email])
     end
 
     it "renderizar corpo" do
-      @mail.body.encoded.should match("#{@user.email} te convidou para participar da republica #{@user.republica.nome}")
-      @mail.body.should have_link("Click aqui para aceitar o convite")
-      @mail.body.encoded.should include(aceitar_convite_url(@convite, token: @convidado.token))
+      mail.body.encoded.should match("#{user.email} te convidou para participar da republica #{user.republica.nome}")
+      mail.body.should have_link("Click aqui para aceitar o convite")
+      mail.body.encoded.should include(aceitar_convite_url(convite, token: convidado.token))
     end
   end
 end
